609782768e95102a966d69479974ffd4a7b441ba
Rasmus Munk <munk1@live.dk>
2021-12-11 23:08:59 +0100

    Add more defaults

--- gocd_tools/defaults.py
+++ gocd_tools/defaults.py
@@ -1,4 +1,6 @@
 import os
+from gocd_tools.utils import is_env_set
+
 
 PACKAGE_NAME = "gocd-utils"
 
@@ -27,3 +29,37 @@ ACCEPT_HEADER_1 = {"Accept": API_VERSION_1, **JSON_HEADER}
 ACCEPT_HEADER_2 = {"Accept": API_VERSION_2, **JSON_HEADER}
 ACCEPT_HEADER_3 = {"Accept": API_VERSION_3, **JSON_HEADER}
 ACCEPT_HEADER_4 = {"Accept": API_VERSION_4, **JSON_HEADER}
+
+
+GO_PLUGINS_BUNDLED_DIR = "/godata"
+GO_SECRET_DIR = "/gosecrets"
+GO_SECRET_DB_FILE = "{}/secrets.yml".format(GO_SECRET_DIR)
+
+# Other constants
+GOCD_SECRET_PLUGIN = "gocd-file-based-secrets-plugin.jar"
+
+
+# TODO, if these ENV vars are not set, use the defaults
+def get_secrets_dir_path():
+    dir_path, msg = is_env_set(GO_SECRET_DIR)
+
+    if not dir_path:
+
+        return False, msg
+    return True, ""
+
+
+def get_secrets_file_name():
+    secrets_name, msg = is_env_set(GO_SECRET_DB_FILE)
+    if not secrets_name:
+        return False, msg
+    return True, ""
+
+
+def get_secrets_db_path():
+    dir_path, msg = get_secrets_dir_path()
+    if not dir_path:
+        return False, msg
+    file_name, msg = get_secrets_file_name()
+    if not file_name:
+        return False, msg
